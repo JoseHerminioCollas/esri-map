e887197e40e6e234d95e992c5ff804dc36f05ac5
joseherminiocollas <joseherminiocollas@goatstone.com>
2016-04-16 18:47:58 -0700

    add bike route layer.

--- js/main.js
+++ js/main.js
@@ -4,37 +4,57 @@
 require([
 		"esri/layers/FeatureLayer",
 		"esri/map",
-		"esri/renderers/HeatmapRenderer" 
+		"esri/renderers/HeatmapRenderer", 
+		"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol", "esri/renderers/SimpleRenderer", 
+		 "esri/Color" 
 	], function ( 
 		FeatureLayer, 
 		Map, 
-		HeatmapRenderer 
+		HeatmapRenderer,
+		SimpleLineSymbol, SimpleFillSymbol, SimpleRenderer,
+		Color
 	){
+
 	var map = new Map("map", {
 		basemap: "gray",
 		zoom: 14,
-		center: [-122.332, 47.606],
+		center: [-122.332, 47.6102],
 		minZoom: 3,
 		maxZoom: 35
 	}) 
-	var serviceURL = "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/SeattleBikes/FeatureServer/0" 
-	var heatmapFeatureLayerOptions = {
-		mode: FeatureLayer.MODE_SNAPSHOT,
+	// bike racks heat map
+	var bikeRackURL = "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/SeattleBikes/FeatureServer/0" 
+	var bikeRackHeatmapLayer = new FeatureLayer( bikeRackURL, {
+		mode: FeatureLayer.MODE_ONDEMAND,
 		outFields: ["*"] 
-	} 
-	var heatmapFeatureLayer = new FeatureLayer(serviceURL, heatmapFeatureLayerOptions) 
+	}  ) 
 	var colors = [ 
 	"rgba(0, 0, 255, 0)", 
 	"rgb(0, 0, 255)",
 	"rgb(255, 0, 255)", 
 	"rgb(255, 0, 0)"
 	]        
-	var heatmapRenderer = new HeatmapRenderer({
+	var bikeRackHeatmapRenderer = new HeatmapRenderer({
 		field: "RACK_CAPAC",
 		colors: colors   
 	}) 
+	// TODO toggle this dynamicmapservices : url do not hae    '/0' 
+	bikeRackHeatmapLayer.setRenderer( bikeRackHeatmapRenderer ) 
 
-	heatmapFeatureLayer.setRenderer(heatmapRenderer) 
-	map.addLayer(heatmapFeatureLayer) 
+	// bike route
+	var symbol = new SimpleLineSymbol();
+	symbol.setWidth( 9 );
+	symbol.setColor(new Color([25,200,0, 0.4 ] ) );
+	var renderer = new SimpleRenderer(symbol);
 
+	var sU = "http://gis.geog.uw.edu:6080/arcgis/rest/services/geogx560/Bike_Routes/MapServer/0" 
+	var routLayer = new FeatureLayer( sU, {
+		mode: FeatureLayer.MODE_ONDEMAND,
+		outFields: ["*"] 
+	} )
+    routLayer.setRenderer( renderer );
+
+    // TODO addLayers( [] ) TODO
+	map.addLayer( bikeRackHeatmapLayer ) 
+	map.addLayer( routLayer )
 }) 
